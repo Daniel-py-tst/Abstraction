31cffd5838f590682b6f7729268a3b9201625613
Daniel-py-tst <82398893+Daniel-py-tst@users.noreply.github.com>
2021-08-15 21:53:50 -0400

    Create ExpSys.lua

---
+++ ExpSys.lua
@@ -0,0 +1,33 @@
+local function levelup(player,amt)
+	local MaxXp = player.leaderstats.MaxXp
+	local Xp = player.leaderstats.Exp
+	local Level = player.leaderstats.Level 
+	
+	local total = Xp.Value + amt
+	if total >= MaxXp.Value then
+		repeat
+			total -= MaxXp.Value
+			Level.Value += 1
+			MaxXp.Value = 100 ^ (1 +(Level.Value * .1))
+		until total < MaxXp.Value
+	end
+	Xp.Value = total	
+end
+
+
+
+local function GiveXp(player, am)
+	local Xp = player.leaderstats.Exp
+	local MaxXp = player.leaderstats.MaxXp
+	local Level = player.leaderstats.Level
+
+	if (am + Xp.Value) >= MaxXp.Value then
+		levelup(player,am)
+	else
+		Xp.Value += am
+	end
+end
+
+script.Parent.ClickDetector.MouseClick:Connect(function(player)
+	GiveXp(player, 200)
+end)
